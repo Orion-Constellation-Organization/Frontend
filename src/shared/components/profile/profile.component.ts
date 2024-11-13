f2eb0e864d7b8b279952901d6a4fb29222078619
Pablo Gomes <pablodamato6@gmail.com>
2024-11-13 16:18:44 -0300

    feature/ambiente-student: Atualizado o profile.component.html inserido os campos de texto e atualizado o campo name, resgatando o username do usuário logado.

--- src/shared/components/profile/profile.component.ts
+++ src/shared/components/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/shared/providers/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userName: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    await this.loadUserData();
+  }
+
+  private async loadUserData(): Promise<void> {
+    try {
+      const userData = await this.authService.getCurrentUser();
+      if (userData && userData.username) {
+        this.userName = userData.username;
+      }
+    } catch (error) {
+      console.error('Erro ao carregar dados do usuário:', error);
+      this.userName = 'Usuário';
+    }
+  }
 }
