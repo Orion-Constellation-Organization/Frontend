7983cf51a0bfacdd0cc3711c33431e366d92d37d
Pablo Gomes <pablodamato6@gmail.com>
2024-11-28 20:11:13 -0300

    feature/lesson-card-manager: Adicionada a Documentação tsdoc na interface schedule.

--- src/shared/interfaces/schedule.interface.ts
+++ src/shared/interfaces/schedule.interface.ts
@@ -1,3 +1,9 @@
+/**
+ * Interface que representa um agendamento.
+ * @interface ISchedule
+ * @property {Date} date - A data do agendamento.
+ * @property {string} time - O horário do agendamento.
+ */
 export interface ISchedule {
   date: Date;
   time: string;
